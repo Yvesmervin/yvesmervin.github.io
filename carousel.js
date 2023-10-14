6a0bad7969705e8b61723b2a43e0064630154df6
Yvesmervin <y.mervin.leroy@gmail.com>
2023-10-13 21:09:12 -0400

    drop down menu for description

--- carousel.js
+++ carousel.js
@@ -1,12 +1,39 @@
+
 var slidePosition = 1;
 SlideShow(slidePosition);
+
+// Add event listeners for touch
+var carousel = document.getElementById('product-image-block'); // Change to your actual carousel id
+carousel.addEventListener('touchstart', handleTouchStart, false);
+carousel.addEventListener('touchmove', handleTouchMove, false);
+
+// Handle touch start
+var xStart = 0;
+function handleTouchStart(event) {
+  xStart = event.touches[0].clientX;
+}
+
+// Handle touch move
+function handleTouchMove(event) {
+  if (!xStart) return;
+  var xEnd = event.touches[0].clientX;
+  var deltaX = xStart - xEnd;
+
+  if (deltaX > 50) {
+    plusSlides(1);  // Swipe left
+  } else if (deltaX < -50) {
+    plusSlides(-1);  // Swipe right
+  }
+
+  xStart = 0;
+}
 
 // forward/Back controls
 function plusSlides(n) {
   SlideShow(slidePosition += n);
 }
 
-//  images controls
+// images controls
 function currentSlide(n) {
   SlideShow(slidePosition = n);
 }
@@ -15,14 +42,23 @@ function SlideShow(n) {
   var i;
   var slides = document.getElementsByClassName("product-image");
   var circles = document.getElementsByClassName("product-carousel-dots");
-  if (n > slides.length) {slidePosition = 1}
-  if (n < 1) {slidePosition = slides.length}
+
+  // Remove 'active' class from all slides and dots
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].classList.remove('active');
+    circles[i].classList.remove('enable');
   }
-  for (i = 0; i < circles.length; i++) {
-      circles[i].className = circles[i].className.replace(" enable", "");
+
+  // Wrap slidePosition around if it goes beyond the slide count
+  if (n > slides.length) {
+    slidePosition = 1;
+  } else if (n < 1) {
+    slidePosition = slides.length;
+  } else {
+    slidePosition = n;
   }
-  slides[slidePosition-1].style.display = "block";
-  circles[slidePosition-1].className += " enable";
+
+  // Display the current slide with animation
+  slides[slidePosition - 1].classList.add('active');
+  circles[slidePosition - 1].classList.add('enable');
 }
